bf6779d1f44456540ed662e411e67963caae7aa2
olto <olto@users.noreply.github.com>
2015-11-18 17:12:24 +0100

    Parentheses for Python 3.x
    import utils
    from lxml import etree, html
    
    def show(line):
        if line[1] == "$HTML":
            print(etree.tostring(html.fromstring(utils.typeDetermin(line[1]), pretty_print=True)))
        else:
            print(utils.typeDetermin(line[1]))

--- httplang/show.py
+++ httplang/show.py
@@ -3,6 +3,6 @@ from lxml import etree, html
 
 def show(line):
     if line[1] == "$HTML":
-        print etree.tostring(html.fromstring(utils.typeDetermin(line[1])), pretty_print=True)
+        print(etree.tostring(html.fromstring(utils.typeDetermin(line[1]), pretty_print=True)))
     else:
-        print utils.typeDetermin(line[1])
+        print(utils.typeDetermin(line[1]))
